b5c90477ac52e1b38e6ba9534959977f1d17f210
Agent-05 <falkwish15@gmail.com>
2024-02-21 13:41:20 -0600

    Copy of Tic-tac-toe with main file added

--- Connect 4/src/Main.java
+++ Connect 4/src/Main.java
@@ -1,5 +1,11 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        
     }
 }
